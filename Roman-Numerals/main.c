9ac907e63d549b9fd74c0e0cdd3125d450cdfcfe
gituJaran <gituJaran-healthca17@currahee.co.uk>
2017-02-19 15:39:14 +0545

    First working version (with acceptance of user input)

--- Roman-Numerals/main.c
+++ Roman-Numerals/main.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+//Runtime_Environment
+#include <limits.h>		/* PATH_MAX */
+
 #define kMaxToConvert            3999
 #define kCardinalOfDigits           4
 #define kASCIIOfsetToIntValue      48
 #define kRomanNumeralDivisorPoints 13
 #define kMaxConsecutiveCharRepeats  3
 
-char* RomanNumerals( void );
-char* ConvertToRoman(int, char*, int[kRomanNumeralDivisorPoints][2][3], int);
-char* ConvertIntArrayToChar(int*, int, char*, int);
+char* Executables_Path( char* );
+char* RomanNumerals( const char* );
+char* ConvertToRoman( int, char*, int[kRomanNumeralDivisorPoints][2][3], int );
+char* ConvertIntArrayToChar( int*, int, char*, int );
 
 int main(int argc, const char * argv[])
 {
-    //RomanNumerals( argc, argv );
-    RomanNumerals();
+    int userArgsOffset = 0;
+    char buffToConvertArg[kCardinalOfDigits +1];  //Four chars for the integer positions plus one for NUL terminating char
+    char filePath[PATH_MAX + 1];
+    
+    if (argc > 0) {
+        if(strstr(argv[0], filePath) != NULL) {
+            userArgsOffset = 1;
+        } //end if
+        strlcpy(buffToConvertArg, argv[userArgsOffset], kCardinalOfDigits+1); //limit the length of whatever user inputs.
+        printf("\n------------------\n%s ---raw input\n-------------------\n", argv[userArgsOffset]);
+        RomanNumerals(buffToConvertArg);
+    } //end if argc > 0 (args length greater than zero
+    
+    
+    
     return 0;
 }
 
-char* RomanNumerals( void ) {
+char* RomanNumerals(const char* buffDecimalToConvert) {
     char buff[kCardinalOfDigits +1];  //Four chars for the integer positions plus one for NUL terminating char
     char resultingRomanNumber[14]; //Longest r.numeral < 4000 is 3488 =  MMMCDLXXXVIII (13 chars long + 1 for NUL terminaing char)
-    int  decimalToConvert = 0, i = kCardinalOfDigits;
+    int  decimalToConvert = 0;
+    unsigned long i, toConvertLen;
     char* buffPointer;
     char c;
     int romanNumerals[kRomanNumeralDivisorPoints][2][3] = {
@@ -42,11 +60,16 @@ char* RomanNumerals( void ) {
     };
     // memset(resultingRomanNumber, '\0', sizeof(resultingRomanNumber));
     //strcat(resultingRomanNumber, "A"); this will be added in at to the left of the first \0 char in string buffer array
-    strcpy(buff, "2016");
-    buffPointer = &buff[0] + kCardinalOfDigits;
+    strcpy(buff, buffDecimalToConvert);
+    //needs to be 0199
+    buffPointer = &buff[0] + strlen(buff);
+    toConvertLen = strlen(buff);
+    i = toConvertLen < kCardinalOfDigits ? toConvertLen : kCardinalOfDigits;
     while ( i > 0 ) {
-        c = *(--buffPointer) - kASCIIOfsetToIntValue;
-        decimalToConvert += (int)pow(10, (double)(kCardinalOfDigits - i)) * c;
+        c = *(--buffPointer);
+        if ( c == '\0' ) { c = '0'; }
+        c -= kASCIIOfsetToIntValue;
+        decimalToConvert += (int)pow(10, (double)(toConvertLen - i)) * c;
         i--;
     }
     printf("\n----%d----\n", decimalToConvert);
@@ -87,6 +110,8 @@ char* ConvertToRoman(int toConvert, char* resultingRomanNumber, int romanNumeral
     } //end if
     return resultingRomanNumber;
 }
+
+
 
 char* ConvertIntArrayToChar(int* pointerToArray, int intArraySize, char* charArrayResult, int charArraySize) {
     int i = 0;
